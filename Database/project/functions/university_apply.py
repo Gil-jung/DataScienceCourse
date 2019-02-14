881699f92b620a58434b6100d131009c7fb8a9b5
Gil-jung <tornado1227@naver.com>
2019-02-14 22:09:35 +0900

    commit

--- Database/project/functions/university_apply.py
+++ Database/project/functions/university_apply.py
@@ -19,20 +19,21 @@ def f1():
                    "ORDER BY university.u_id ")
             cursor.execute(sql)
             result = cursor.fetchall()
+            df = pd.DataFrame(result)
+            cols_order = ['u_id', 'u_name', 'capacity', 'u_group', 'cutline', 'weight', 'applied']
+            df = df[cols_order]
+            newcols = {'u_id': 'id', 'u_name': 'name', 'u_group': 'group' }
+            print(df.rename(columns = newcols).to_string(index=False))
+            print()
 
     except ValueError:
         print("There is no records. Please insert university record by press 3")
         print()
+
     finally:
         connection.close()
 
-    df = pd.DataFrame(result)
-    cols_order = ['u_id', 'u_name', 'capacity', 'u_group', 'cutline', 'weight', 'applied']
-    df = df[cols_order]
-    newcols = {'u_id': 'id', 'u_name': 'name', 'u_group': 'group' }
-    # df.sort_index(axis=1)
-    print(df.rename(columns = newcols).to_string(index=False))
-    print()
+
 
 
 def f2():
@@ -52,20 +53,21 @@ def f2():
                    "ORDER BY s_id ")
             cursor.execute(sql)
             result = cursor.fetchall()
+            df = pd.DataFrame(result)
+            cols_order = ['s_id', 's_name', 'csat_score', 'school_score']
+            df = df[cols_order]
+            newcols = {'s_id': 'id', 's_name': 'name'}
+            print(df.rename(columns = newcols).to_string(index=False))
+            print()
 
     except ValueError:
         print("There is no records. Please insert student record by press 5")
         print()
+
     finally:
         connection.close()
 
-    df = pd.DataFrame(result)
-    cols_order = ['s_id', 's_name', 'csat_score', 'school_score']
-    df = df[cols_order]
-    newcols = {'s_id': 'id', 's_name': 'name'}
-    # df.sort_index(axis=1)
-    print(df.rename(columns = newcols).to_string(index=False))
-    print()
+
 
 
 def f3():
@@ -92,9 +94,11 @@ def f3():
             connection.commit()
         print("A university is successfully inserted.")
         print()
+
     except ValueError:
         print("You insert wrong data. a university doesn't updated")
         print()
+
     finally:
         connection.close()
 
@@ -118,7 +122,6 @@ def f4():
                     "WHERE u_id = %s " )
             cursor.execute(sql1, u_id)
             result1 = cursor.fetchall()
-            # print(result1[0]['u_id'])
             if result1[0]['u_id'] == u_id :
                 sql = ("DELETE FROM university "
                        "       WHERE u_id = %s ")
@@ -126,9 +129,11 @@ def f4():
                 connection.commit()
         print("A university is successfully removed.")
         print()
+
     except IndexError:
         print("A university is not founded")
         print()
+
     finally:
         connection.close()
 
@@ -155,9 +160,11 @@ def f5():
             connection.commit()
         print("A student is successfully inserted.")
         print()
+
     except ValueError:
         print("You insert wrong data. a student doesn't updated")
         print()
+
     finally:
         connection.close()
 
@@ -187,11 +194,13 @@ def f6():
                        "       WHERE s_id = %s ")
                 cursor.execute(sql, s_id)
                 connection.commit()
-        print("A student is successfully removed.")
-        print()
+                print("A student is successfully removed.")
+                print()
+
     except IndexError:
         print("A student is not founded")
         print()
+
     finally:
         connection.close()
 
@@ -211,24 +220,34 @@ def f7():
 
     try:
         with connection.cursor() as cursor:
-            sql1 = ("INSERT INTO apply(s_id, u_id ) "
-                   "    VALUES(%s, %s) ")
-            sql2 = ("SELECT count(u_group) as count "
+            sql1 = ("SELECT u_group, count(u_group) as count "
                     "FROM university natural join apply "
-                    "WHERE s_id = %s and u_id = %s ")
+                    "WHERE s_id = %s and u_id = %s "
+                    "GROUP BY u_group " )
             cursor.execute(sql1, (s_id, u_id))
-            connection.commit()
-            cursor.execute(sql2, (s_id, u_id))
             result = cursor.fetchall()
-            # if result > 1:
-            #     connection.rollback()
-            #     print("This university group already applied")
-            # else:
-            #     print("Successfully made an application.")
-            #     print()
-    # except ValueError:
-    #     print("You insert wrong data. a student doesn't updated")
-    #     print()
+            if result[0]['count'] > 0 :
+                print("This university group already applied")
+                print()
+
+    except IndexError:
+        try:
+            with connection.cursor() as cursor:
+                sql2 = ("INSERT INTO apply(s_id, u_id ) "
+                        "    VALUES(%s, %s) ")
+                cursor.execute(sql2, (s_id, u_id))
+                connection.commit()
+                print("Successfully made an application.")
+                print()
+
+        except IndexError:
+            print("The student doesn't exist. Please insert student record by press 5")
+            print()
+
+    except ValueError:
+        print("You insert wrong data. a student doesn't updated")
+        print()
+
     finally:
         connection.close()
 
@@ -253,20 +272,19 @@ def f8():
                    "ORDER BY s_id ")
             cursor.execute(sql, u_id)
             result = cursor.fetchall()
+            df = pd.DataFrame(result)
+            cols_order = ['s_id', 's_name', 'csat_score', 'school_score']
+            df = df[cols_order]
+            newcols = {'s_id': 'id', 's_name': 'name'}
+            print(df.rename(columns = newcols).to_string(index=False))
+            print()
 
     except ValueError:
         print("There is no university apply records. Please insert apply record by press 7")
         print()
+
     finally:
         connection.close()
-
-    df = pd.DataFrame(result)
-    cols_order = ['s_id', 's_name', 'csat_score', 'school_score']
-    df = df[cols_order]
-    newcols = {'s_id': 'id', 's_name': 'name'}
-    # df.sort_index(axis=1)
-    print(df.rename(columns = newcols).to_string(index=False))
-    print()
 
 
 def f9():
@@ -283,27 +301,30 @@ def f9():
 
     try:
         with connection.cursor() as cursor:
-            sql = ("SELECT university.*, count(apply.u_id) as applied "
+            sql = ("SELECT university.*, a.applied as applied "
                    "FROM university natural join apply "
+                   "                natural join "
+                   "     ( SELECT u_id, count(apply.u_id) as applied "
+                   "       FROM university natural join apply "
+                   "       GROUP BY university.u_id ) a "
                    "WHERE s_id = %s "
                    "GROUP BY university.u_id, university.u_name, university.capacity, university.u_group, university.cutline, university.weight "
                    "ORDER BY university.u_id ")
             cursor.execute(sql, s_id)
             result = cursor.fetchall()
+            df = pd.DataFrame(result)
+            cols_order = ['u_id', 'u_name', 'capacity', 'u_group', 'cutline', 'weight', 'applied']
+            df = df[cols_order]
+            newcols = {'u_id': 'id', 'u_name': 'name', 'u_group': 'group' }
+            print(df.rename(columns = newcols).to_string(index=False))
+            print()
 
     except ValueError:
         print("There is no student apply records. Please insert apply record by press 7")
         print()
+
     finally:
         connection.close()
-
-    df = pd.DataFrame(result)
-    cols_order = ['u_id', 'u_name', 'capacity', 'u_group', 'cutline', 'weight', 'applied']
-    df = df[cols_order]
-    newcols = {'u_id': 'id', 'u_name': 'name', 'u_group': 'group' }
-    # df.sort_index(axis=1)
-    print(df.rename(columns = newcols).to_string(index=False))
-    print()
 
 
 def f10():
@@ -322,31 +343,35 @@ def f10():
         with connection.cursor() as cursor:
             sql = ("SELECT a.s_id, a.s_name, a.school_score, a.csat_score "
                    "FROM ( "
-                   "	SELECT *, @curRank := @curRank + 1 as rank "
+                   "SELECT *, case when counter > ceiling(capacity * 1.1) then 'Fail' else 'Pass' end as flag "
+                   "FROM ( "
+                   "	SELECT * "
+                   "    FROM ( "
+                   "    SELECT *, case when @prev_value1 = csat_score + (school_score * weight) and @prev_value2 = school_score then @vRank  else @vRank := @vRank + 1 end as rank, @vCounter := @vCounter + 1 as counter, @prev_value1 := csat_score + (school_score * weight), @prev_value2 := school_score "
                    "	FROM university natural join apply "
                    "					natural join student, "
-                   "		(SELECT @curRank := 0) r "
+                   "		(SELECT @vRank := 0, @vCounter := 0, @prev_value1 := NULL, @prev_value2 := NULL) r "
                    "	WHERE "
                    "		apply.u_id = %s and "
                    "		cutline <= csat_score + (school_score * weight) "
-                   "	ORDER BY csat_score + (school_score * weight) desc ) a "
-                   "WHERE rank <= capacity ")
+                   "	ORDER BY csat_score + (school_score * weight) desc, school_score desc ) b ) c "
+                   "WHERE rank <= capacity ) a "
+                   "WHERE flag = 'Pass' " )
             cursor.execute(sql, u_id)
             result = cursor.fetchall()
+            df = pd.DataFrame(result)
+            cols_order = ['s_id', 's_name', 'csat_score', 'school_score']
+            df = df[cols_order]
+            newcols = {'s_id': 'id', 's_name': 'name'}
+            print(df.rename(columns = newcols).to_string(index=False))
+            print()
 
     except ValueError:
         print("There is no university apply records. Please insert apply record by press 7")
         print()
+
     finally:
         connection.close()
-
-    df = pd.DataFrame(result)
-    cols_order = ['s_id', 's_name', 'csat_score', 'school_score']
-    df = df[cols_order]
-    newcols = {'s_id': 'id', 's_name': 'name'}
-    # df.sort_index(axis=1)
-    print(df.rename(columns = newcols).to_string(index=False))
-    print()
 
 
 def f11():
@@ -363,34 +388,41 @@ def f11():
 
     try:
         with connection.cursor() as cursor:
-            sql = ("SELECT a.u_id, a.u_name, a.capacity, a.u_group, a.cutline, a.weight, count(a.u_id) as applied "
+            sql = ("SELECT a.u_id, a.u_name, a.capacity, a.u_group, a.cutline, a.weight, f.applied "
                    "FROM ( "
-                   "	SELECT *, @curRank := @curRank + 1 as rank "
+                   "SELECT *, case when counter > ceiling(capacity * 1.1) then 'Fail' else 'Pass' end as flag "
+                   "FROM ( "
+                   "	SELECT * "
+                   "    FROM ( "
+                   "    SELECT *, case when @prev_value1 = csat_score + (school_score * weight) and @prev_value2 = school_score then @vRank  else @vRank := @vRank + 1 end as rank, @vCounter := @vCounter + 1 as counter, @prev_value1 := csat_score + (school_score * weight), @prev_value2 := school_score "
                    "	FROM university natural join apply "
                    "					natural join student, "
-                   "		(SELECT @curRank := 0) r "
+                   "		(SELECT @vRank := 0, @vCounter := 0, @prev_value1 := NULL, @prev_value2 := NULL) r "
                    "	WHERE "
                    "		apply.s_id = %s and "
                    "		cutline <= csat_score + (school_score * weight) "
-                   "	ORDER BY csat_score + (school_score * weight) desc ) a "
-                   "WHERE rank <= capacity "
+                   "	ORDER BY csat_score + (school_score * weight) desc, school_score desc ) b ) c "
+                   "WHERE rank <= capacity ) a natural join "
+                   "     ( SELECT u_id, count(apply.u_id) as applied "
+                   "       FROM university natural join apply "
+                   "       GROUP BY university.u_id ) f "
+                   "WHERE flag = 'Pass' "
                    "GROUP BY a.u_id, a.u_name, a.capacity, a.u_group, a.cutline, a.weight ")
             cursor.execute(sql, s_id)
             result = cursor.fetchall()
+            df = pd.DataFrame(result)
+            cols_order = ['u_id', 'u_name', 'capacity', 'u_group', 'cutline', 'weight', 'applied']
+            df = df[cols_order]
+            newcols = {'u_id': 'id', 'u_name': 'name', 'u_group': 'group' }
+            print(df.rename(columns = newcols).to_string(index=False))
+            print()
 
     except ValueError:
         print("There is no student apply records. Please insert apply record by press 7")
         print()
+
     finally:
         connection.close()
-
-    df = pd.DataFrame(result)
-    cols_order = ['u_id', 'u_name', 'capacity', 'u_group', 'cutline', 'weight', 'applied']
-    df = df[cols_order]
-    newcols = {'u_id': 'id', 'u_name': 'name', 'u_group': 'group' }
-    # df.sort_index(axis=1)
-    print(df.rename(columns = newcols).to_string(index=False))
-    print()
 
 
 def f13():
@@ -415,7 +447,7 @@ def f13():
                     "    capacity    	int				check (capacity >= 1), "
                     "    u_group		char(1) 		check (u_group in ('A', 'B', 'C')), "
                     "    cutline		int				check (cutline >= 0), "
-                    "    weight			numeric(1,1)	check (weight >= 0), "
+                    "    weight			numeric(2,1)	check (weight >= 0), "
                     "    primary key (u_id) "
                     "); ")
             sql5 = ("create table student "
@@ -443,10 +475,6 @@ def f13():
             cursor.execute(sql5)
             cursor.execute(sql6)
             connection.commit()
-            # result = cursor.fetchall()
-            # df = pd.DataFrame(result)
-            # print(df)
-            # print()
             print("Successfully reset database.")
             print()
     except :
@@ -454,4 +482,3 @@ def f13():
         print()
     finally:
         connection.close()
-    print()
